54c5d4d59f47f7605d2547b16f5903c046b15917
Hoang Khoi <59761477+lehoangkhoi01@users.noreply.github.com>
2022-10-06 08:53:06 +0700

    Update migration and database

---
+++ Club-Event-Management/Infrastructure/Migrations/20221006014715_AddFieldForStudentAccount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddFieldForStudentAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentId",
+                table: "StudentAccounts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "StudentAccounts");
+        }
+    }
+}
